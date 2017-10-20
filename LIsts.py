462f4952344a29078712ae184c5c7fe3ece6de60
HALLASTERA <32964124+HALLASTERA@users.noreply.github.com>
2017-10-21 02:23:42 +0300

    Add files via upload

--- LIsts.py
+++ LIsts.py
@@ -1,25 +1,34 @@
-cities = ['bryanks','Almati', 'luhanks', 'Moscow', 'Kiev']
-print(cities)
-print(len(cities))
-print(cities[2])
-print(cities[-2])
-print(cities[2].title())
-print(cities[2].upper())
-cities[3] = 'Orel'
-print(cities)
-cities.append('Domodedovo')
-print(cities)
-cities.append('Novosib')
-print(cities)
-cities.insert(3, 'Militopol')
-print(cities)
+#           0        1        2        3        4
+fruits = ['Yabloko','banan','Viwnya','Sliva','Persik'] #Массив фруктов
 
-del cities[3]
-print(cities)
-cities.remove('Kiev')
-print(cities)
-deleted_city = cities.pop()
-print("Deleted city is: " + deleted_city)
-print(cities)
-cities.reverse()
-print(cities)
+print(fruits)
+print(len(fruits)) # "len" какой длины массив
+print(fruits[4])
+print(fruits[1].upper()) #.upper() делает большими буквами массив
+print(fruits[-2]) # показывает второй элемент массива с конца
+fruits[1] = 'qiwi' # заменяет элемент массива на другой
+print(fruits)
+fruits.append('4ernosliv') # dobavlyaet v konec massiva novui element
+print(fruits)
+fruits.append('Moloko')
+print(fruits)
+fruits.insert(0,'vinograd') # dobavlyaet na nomer massiva nujnui element insert dobavi't
+fruits.insert(4,'morkov')
+print(fruits)
+print(fruits[0].upper())
+print(fruits)
+
+del fruits[2] # удаление элемента массива по номеру
+print(fruits)
+fruits.remove('Viwnya') # удаление элемента массива по названию
+print(fruits)
+
+deleted_fruits = fruits.pop(2) #deleted_nazvanie = nazanive_pop() udalenie  elementa i ego pokaz
+print("deleted fruits is:" + deleted_fruits )
+fruits.sort() # sortirovka po alfavitu
+print(fruits)
+
+fruits.sort(reverse=True) # sortirovka po drugomu randomno
+print(fruits)
+fruits.reverse() # sortirovka naoborot
+print(fruits)
